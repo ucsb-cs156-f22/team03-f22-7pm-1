a8f6ef4ce58d3096f316f79ef794327165f5e6e8
KennethBuii <khbui@ucsb.edu>
2022-11-10 15:01:41 -0800

    replaced place holder page with table

---
+++ frontend/src/fixtures/menuItemFixtures.js
@@ -0,0 +1,27 @@
+const menuItemFixtures = {
+    oneMenuItem: {
+            "diningCommonsCode": "Ortega",
+            "name": "pizza",
+            "station": "yummy entree"
+    },
+    threeMenuItem: [
+        {
+            "diningCommonsCode": "Ortega",
+            "name": "pizza",
+            "station": "yummy entree"
+        },
+        {
+            "diningCommonsCode": "Ortega",
+            "name": "hotdog",
+            "station": "entree"
+        },
+        {
+            "diningCommonsCode": "Portola",
+            "name": "pho",
+            "station": "special entree"
+        }
+    ]
+};
+
+
+export { menuItemFixtures };
