b27d8559ea5f164d4bcc716677129f692cfd9c71
KennethBuii <khbui@ucsb.edu>
2022-11-10 17:34:57 -0800

    fixed team02 error

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
@@ -1,7 +1,9 @@
 package edu.ucsb.cs156.example.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,8 @@ import lombok.Builder;
 
 public class UCSBDiningCommonsMenuItem {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String diningCommonsCode;
     private String name;
     private String station;
