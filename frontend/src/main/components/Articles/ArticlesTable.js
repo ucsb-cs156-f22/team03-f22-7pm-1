15a2473bf4bbaffff97051d2d42c0adffd537d49
Junhwan Lee <junhwanlee2316@gmail.com>
2022-11-15 19:49:23 -0800

    utils update

--- frontend/src/main/components/Articles/ArticlesTable.js
+++ frontend/src/main/components/Articles/ArticlesTable.js
@@ -1,8 +1,18 @@
 import OurTable, { ButtonColumn } from "main/components/OurTable";
 import { useBackendMutation } from "main/utils/useBackend";
-import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/ArticlesUtils"
+import { onDeleteSuccess } from "main/utils/UCSBDateUtils";
 //import { useNavigate } from "react-router-dom";
 import { hasRole } from "main/utils/currentUser";
+
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/articles",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
+    }
+}
 
 export default function ArticlesTable({ articles, currentUser }) {
 
